ca82045e6773d076c9cd14a6e4c60739b3741a59
cgiacometti <camilla.giacometti@gmail.com>
2022-04-18 08:12:20 -0300

    carregar chat, exibir mensagens conforme layout

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let nome = prompt("Qual o seu nome?");
+const chat = document.querySelector(".chat");
 
 entrada();
 
@@ -10,10 +11,48 @@ function entrada(){
 }
 
 function tratarSucesso(resposta){
-    console.log(resposta);
+    console.log(resposta); //lembrar de tirar
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(carregarChat);
 }
 
 function tratarErro(erro){
-    prompt("Esse nome já está em uso. Por favor, escolha outro nome:");
-    entrada();
+    if(erro.response.status === 400){
+        prompt("Esse nome já está em uso. Por favor, escolha outro nome:");
+        entrada();
+    }
+}
+
+function carregarChat(resposta){
+    const mensagens = resposta.data;
+    const qntMensagens = mensagens.length;
+    console.log(mensagens);
+    for(let i = 0; i < qntMensagens; i++){
+        if(mensagens[i].type === "status"){
+            chat.innerHTML += `<div class="status">
+                                <span class="time">(${mensagens[i].time})</span>
+                                <span class="user">${mensagens[i].from}</span>
+                                <span class="text">${mensagens[i].text}</span>
+                            </div>`
+        }
+        if(mensagens[i].type === "message"){
+            if(mensagens[i].to === "Todos"){
+                chat.innerHTML += `<div class="msgTodos">
+                                <span class="time">(${mensagens[i].time})</span>
+                                <span class="user">${mensagens[i].from}</span>
+                                <span class="text">para</span>
+                                <span class="user">${mensagens[i].to}<span class="text">:</span></span>
+                                <span class="text">${mensagens[i].text}</span>
+                            </div>`
+            } else {
+                chat.innerHTML += `<div class="msgReservada">
+                                <span class="time">(${mensagens[i].time})</span>
+                                <span class="user">${mensagens[i].from}</span>
+                                <span class="text">reservadamente para</span>
+                                <span class="user">${mensagens[i].to}<span class="text">:</span></span>
+                                <span class="text">${mensagens[i].text}</span>
+                            </div>`
+            }
+        }
+    }
 }
